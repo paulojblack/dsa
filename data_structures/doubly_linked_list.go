4f4bc2a30ffbd1bec39021740a632dff53f1c10d
Paulo Black <paulo@pauloblack.com>
2024-09-07 14:45:56 -0400

    doubly linked list, writing these tests got so boring

---
+++ data_structures/doubly_linked_list.go
@@ -0,0 +1,107 @@
+package ds
+
+import (
+	"errors"
+)
+
+type Node struct {
+	Val  int
+	Next *Node
+	Prev *Node
+}
+
+type DLL struct {
+	head   *Node
+	length int
+}
+
+func CreateDLL() DLL {
+
+	return DLL{
+		length: 0,
+		head:   nil,
+	}
+}
+
+func (l *DLL) prepend(item int) {
+	node := Node{
+		Val: item,
+	}
+	l.length++
+	if l.head == nil {
+		l.head = &node
+		return
+	}
+
+	node.Next = l.head
+	l.head.Prev = &node
+	l.head = &node
+}
+
+func (l *DLL) insertAt(item int, idx int) error {
+	if idx > l.length {
+		return errors.New("attempting to insert at idx larger than DLL size")
+	}
+
+	// index 0 is just a prepend
+	if idx == 0 {
+		l.prepend(item)
+		return nil
+	}
+
+	cur := l.head
+
+	for i := 1; i < idx; i++ {
+		cur = cur.Next
+	}
+
+	node := Node{
+		Val:  item,
+		Next: cur.Next,
+		Prev: cur,
+	}
+	if cur.Next != nil {
+		cur.Next.Prev = &node
+	}
+	cur.Next = &node
+	l.length++
+
+	return nil
+}
+
+func (l *DLL) append(item int) error {
+	node := Node{
+		Val: item,
+	}
+
+	if l.head == nil {
+		l.head = &node
+		return nil
+	}
+
+	err := l.insertAt(item, l.length)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (l *DLL) remove(item int) {
+}
+func (l *DLL) removeAt(item int, idx int) {
+
+}
+func (l *DLL) get(idx int) (int, error) {
+	if idx >= l.length {
+		return -1, errors.New("attempting to get at idx larger than DLL size")
+	}
+
+	cur := l.head
+	for i := 0; i < idx; i++ {
+		cur = cur.Next
+	}
+
+	return cur.Val, nil
+}
---
+++ data_structures/doubly_linked_list_test.go
@@ -0,0 +1,147 @@
+package ds
+
+import (
+	"fmt"
+	"testing"
+)
+
+func setupDLL() DLL {
+	dll := CreateDLL()
+	sample := []int{15, 2, 1, 5, 19, 2, 3, 4, 192, 4}
+
+	for _, v := range sample {
+		dll.prepend(v)
+	}
+
+	return dll
+}
+
+func TestDLLAppend(t *testing.T) {
+	dll := setupDLL()
+
+	err := dll.append(1000)
+
+	if err != nil {
+		t.Errorf("failed to append with error %v", err)
+	}
+
+	if dll.length != 11 {
+		t.Errorf("incorrect length, received %v", 11)
+	}
+
+	if v, _ := dll.get(10); v != 1000 {
+		t.Errorf("wrong value at last index, expected 1000 received %v", v)
+	}
+
+}
+
+func TestDLLInsertAt(t *testing.T) {
+	dll := setupDLL()
+
+	dll.insertAt(1000, 1)
+
+	if dll.length != 11 {
+		t.Errorf("failed to increment length in append")
+	}
+
+	if dll.head.Next.Val != 1000 {
+		t.Errorf("incorrect head.Next: %v", dll.head.Next)
+	}
+
+	if dll.head.Next.Prev != dll.head {
+		t.Errorf("incorrect prev: %v", dll.head.Next.Prev)
+	}
+
+	err := dll.insertAt(99, 11)
+
+	if err != nil {
+		t.Errorf("Err %v while inserting at end of DLL", err)
+	}
+
+	if dll.length != 12 {
+		t.Errorf("failed to insert at end of dll")
+	}
+
+	if v, _ := dll.get(11); v != 99 {
+		t.Errorf("did not find value 99 at last index, instead: %v", v)
+	}
+
+}
+
+func TestDLLInsertAtHead(t *testing.T) {
+	dll := setupDLL()
+
+	fmt.Println(dll.length)
+
+	dll.insertAt(1000, 0)
+
+	if dll.length != 11 {
+		t.Errorf("failed to increment length in append")
+	}
+
+	if dll.head.Val != 1000 {
+		t.Errorf("wrong value at head")
+	}
+
+	if dll.head.Next.Val != 4 {
+		t.Errorf("incorrect head.Next: %v", dll.head.Next)
+	}
+}
+
+func TestDLLPrepend(t *testing.T) {
+	// testArr := []int{15, 2, 1, 5, 19, 2, 3, 4, 192, 4}
+
+	dll := CreateDLL()
+
+	dll.prepend(1)
+
+	if dll.length != 1 {
+		t.Errorf("failed to prepend first element to DLL")
+	}
+
+	dll.prepend(2)
+
+	if dll.length != 2 {
+		t.Errorf("failed to prepend second element to DLL")
+	}
+
+	if dll.head.Next == nil || dll.head.Next.Val != 1 {
+		t.Errorf("incorrect Next assignment in prepend of second DLL element")
+	}
+
+	if dll.head.Next.Prev != dll.head {
+		t.Errorf("incorrect Prev assignment during prepnd of second DLL element")
+	}
+
+	fmt.Println("correctly prepended two elements to DLL")
+}
+
+func TestDLLGet(t *testing.T) {
+	dll := setupDLL()
+
+	val, err := dll.get(4)
+
+	if err != nil {
+		t.Errorf("error getting expected value 4 %v", err)
+	}
+
+	if val != 2 {
+		t.Errorf("incorrectly received %v instead of 2", val)
+	}
+
+	val, err = dll.get(9)
+
+	if err != nil {
+		t.Errorf("error getting expected value 15 %v", err)
+	}
+
+	if val != 15 {
+		t.Errorf("incorrectly received %v instead of 15", val)
+	}
+
+	_, err = dll.get(10)
+
+	if err == nil {
+		t.Errorf("did not correctly throw error when attempting to access index 10 beyond length %v", dll.length)
+	}
+}
